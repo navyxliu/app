b5a0767ff953fdd5c4623a8b92f93ba8f2f0e28c
navyxliu <navy.xliu@gmail.com>
2013-02-05 21:41:38 +0800

    changed for gfan

--- my-build.sh
+++ my-build.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-./jni/jni.sh 
 javah -classpath ./bin/classes -d jni/ com.droidmame.sf2.Emulator 
 $ANDROID_NDK/ndk-build APP_ABI="armeabi-v7a"
 cp ~/Development/droidmame/libdroidMAME.so ./libs/armeabi-v7a/libMAME4all.so
